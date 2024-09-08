660660ba8e6c9122266de2e5fd642c1b505052a0
wehem <kiran228@proton.me>
2024-09-08 21:59:32 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,28 @@
 #include <windows.h>
-#include <dxgi1_3.h>
-#include <d3d11.h>
+#include <d3d12.h>
+#include <dxgi1_4.h>
 #include <chrono>
-#include <memory>
-#include <string>
+
 #include "strobe-api/strobe/strobe-core.h"
 
+#pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxgi.lib")
-#pragma comment(lib, "d3d11.lib")
 
 using namespace std::chrono;
 
 const char g_szClassName[] = "desktopBFIwindowClass";
 bool quitProgram = false;
-bool frameVisible = true;
+bool frameVisible;
 
-std::unique_ptr<StrobeAPI> strobe;
-std::string strobeInfo;
+StrobeAPI* strobe = nullptr;
+char* strobeInfo = (char*)malloc(sizeof(char) * 4096);
 bool debugMode = false;
 int frameSnapshot = 0;
 
+ID3D12Device* d3d12Device = nullptr;
+IDXGIFactory4* dxgiFactory = nullptr;
+IDXGIAdapter1* dxgiAdapter = nullptr;
 IDXGIOutput* dxgiOutput = nullptr;
-IDXGISwapChain1* swapChain = nullptr;
 
 // Window event handling
 LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
@@ -32,17 +33,18 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
     {
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
-        if (strobe && debugMode && !strobeInfo.empty() && strobe->frameCount(StrobeAPI::TotalFrame) - frameSnapshot >= 10)
+        if (strobe && debugMode && strobeInfo && strobe->frameCount(StrobeAPI::TotalFrame) - frameSnapshot >= 10)
         {
             RECT rec;
             SetRect(&rec, 10, 10, 500, 600);
             HBRUSH brush = CreateSolidBrush(RGB(255, 255, 255));
             FillRect(hdc, &rec, brush);
-            DeleteObject(brush);
-            DrawTextA(hdc, strobeInfo.c_str(), -1, &rec, DT_TOP | DT_LEFT);
+            snprintf(strobeInfo, sizeof(char) * 4096, "%s", strobe->getDebugInformation());
+            DrawText(hdc, strobeInfo, strlen(strobeInfo), &rec, DT_TOP | DT_LEFT);
             frameSnapshot = strobe->frameCount(StrobeAPI::TotalFrame);
         }
         EndPaint(hwnd, &ps);
+        ReleaseDC(hwnd, hdc);
         break;
     }
     case WM_CLOSE:
@@ -59,118 +61,121 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
     return 0;
 }
 
-bool CreateMainWindow(HINSTANCE hInstance, int nCmdShow, HWND& hwnd)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+    LPSTR lpCmdLine, int nCmdShow)
 {
-    WNDCLASSEX wc = { sizeof(WNDCLASSEX), 0, WndProc, 0, 0, hInstance,
-                      LoadIcon(NULL, IDI_APPLICATION), LoadCursor(NULL, IDC_ARROW),
-                      (HBRUSH)GetStockObject(BLACK_BRUSH), NULL, g_szClassName,
-                      LoadIcon(NULL, IDI_APPLICATION) };
+    SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL);
+
+    WNDCLASSEX wc;
+    HWND hwnd;
+    MSG Msg;
+
+    // Registering the Window Class
+    wc.cbSize = sizeof(WNDCLASSEX);
+    wc.style = 0;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = g_szClassName;
+    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 
     if (!RegisterClassEx(&wc))
     {
-        MessageBox(NULL, "Window Registration Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
-        return false;
+        MessageBox(NULL, "Window Registration Failed!", "Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
     }
 
-    hwnd = CreateWindowEx(WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOPMOST,
-        g_szClassName, "DesktopBFI", WS_POPUP,
+    // Creating the Window
+    hwnd = CreateWindowEx(
+        WS_EX_LAYERED | WS_EX_TRANSPARENT | WS_EX_TOPMOST,
+        g_szClassName,
+        "DesktopBFI",
+        WS_POPUP,
         0, 0, GetSystemMetrics(SM_CXSCREEN), GetSystemMetrics(SM_CYSCREEN),
         NULL, NULL, hInstance, NULL);
 
     if (hwnd == NULL)
     {
-        MessageBox(NULL, "Window Creation Failed!", "Error!", MB_ICONEXCLAMATION | MB_OK);
-        return false;
+        MessageBox(NULL, "Window Creation Failed!", "Error!",
+            MB_ICONEXCLAMATION | MB_OK);
+        return 0;
     }
 
     ShowWindow(hwnd, nCmdShow);
     UpdateWindow(hwnd);
 
-    return true;
-}
+    LPWSTR* szArglist;
+    int argCount;
+    szArglist = CommandLineToArgvW(GetCommandLineW(), &argCount);
+    if (argCount == 4)
+    {
+        int sMode = _wtoi(szArglist[1]);
+        int pSInterval = _wtoi(szArglist[2]);
+        debugMode = _wtoi(szArglist[3]);
 
-bool SetupDXGI(HWND hwnd)
-{
-    IDXGIFactory2* factory = nullptr;
-    ID3D11Device* device = nullptr;
-    ID3D11DeviceContext* context = nullptr;
+        strobe = new StrobeCore(sMode, pSInterval);
+    }
+    else
+    {
+        strobe = new StrobeCore();
+    }
 
-    HRESULT hr = CreateDXGIFactory2(0, __uuidof(IDXGIFactory2), (void**)&factory);
+    // Initialize DirectX 12
+    HRESULT hr = CreateDXGIFactory1(IID_PPV_ARGS(&dxgiFactory));
     if (FAILED(hr))
     {
         MessageBox(NULL, "Failed to create DXGI Factory!", "Error!", MB_ICONEXCLAMATION | MB_OK);
-        return false;
+        return 0;
     }
 
-    hr = D3D11CreateDevice(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, 0, nullptr, 0,
-                           D3D11_SDK_VERSION, &device, nullptr, &context);
+    hr = dxgiFactory->EnumAdapters1(0, &dxgiAdapter);
     if (FAILED(hr))
     {
-        MessageBox(NULL, "Failed to create D3D11 Device!", "Error!", MB_ICONEXCLAMATION | MB_OK);
-        factory->Release();
-        return false;
+        MessageBox(NULL, "Failed to enumerate DXGI Adapter!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
     }
 
-    DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
-    swapChainDesc.BufferCount = 2;
-    swapChainDesc.Width = 0;
-    swapChainDesc.Height = 0;
-    swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-    swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_SEQUENTIAL;
-    swapChainDesc.SampleDesc.Count = 1;
-
-    hr = factory->CreateSwapChainForHwnd(device, hwnd, &swapChainDesc, nullptr, nullptr, &swapChain);
+    hr = dxgiAdapter->EnumOutputs(0, &dxgiOutput);
     if (FAILED(hr))
     {
-        MessageBox(NULL, "Failed to create Swap Chain!", "Error!", MB_ICONEXCLAMATION | MB_OK);
-        device->Release();
-        context->Release();
-        factory->Release();
-        return false;
+        MessageBox(NULL, "Failed to enumerate DXGI Output!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
     }
 
-    IDXGIAdapter* adapter = nullptr;
-    hr = factory->EnumAdapters(0, &adapter);
+    hr = D3D12CreateDevice(dxgiAdapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&d3d12Device));
     if (FAILED(hr))
     {
-        MessageBox(NULL, "Failed to enumerate adapters!", "Error!", MB_ICONEXCLAMATION | MB_OK);
-        swapChain->Release();
-        device->Release();
-        context->Release();
-        factory->Release();
-        return false;
+        MessageBox(NULL, "Failed to create D3D12 Device!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+        return 0;
     }
-
-    hr = adapter->EnumOutputs(0, &dxgiOutput);
-    if (FAILED(hr))
-    {
-        MessageBox(NULL, "Failed to enumerate outputs!", "Error!", MB_ICONEXCLAMATION | MB_OK);
-        adapter->Release();
-        swapChain->Release();
-        device->Release();
-        context->Release();
-        factory->Release();
-        return false;
-    }
-
-    adapter->Release();
-    device->Release();
-    context->Release();
-    factory->Release();
-
-    return true;
-}
-
-int RunMainLoop(HWND hwnd)
-{
-    MSG msg;
-    DXGI_OUTPUT_DESC outputDesc;
-    dxgiOutput->GetDesc(&outputDesc);
 
     while (!quitProgram)
     {
-        dxgiOutput->WaitForVBlank();
+        // Wait for VBlank
+        hr = dxgiOutput->WaitForVBlank();
+        if (FAILED(hr))
+        {
+            MessageBox(NULL, "Failed to wait for VBlank!", "Error!", MB_ICONEXCLAMATION | MB_OK);
+            break;
+        }
+
+        // Poll for VBlank exit
+        BOOL inVBlank = TRUE;
+        do {
+            high_resolution_clock::time_point pollTime = high_resolution_clock::now() + microseconds(100);
+            while (pollTime > high_resolution_clock::now())
+            {
+            }
+            DXGI_OUTPUT_DESC outputDesc;
+            dxgiOutput->GetDesc(&outputDesc);
+            inVBlank = outputDesc.DesktopCoordinates.bottom == 0; // Assuming VBlank when desktop coordinates are 0
+        } while (inVBlank);
 
         if (strobe)
             frameVisible = strobe->strobe();
@@ -180,81 +185,48 @@ int RunMainLoop(HWND hwnd)
         // Window transparency: 0 is invisible, 255 is opaque
         SetLayeredWindowAttributes(hwnd, 0, (frameVisible ? 0 : 1) * 255, LWA_ALPHA);
         RedrawWindow(hwnd, NULL, NULL, RDW_INVALIDATE);
-
-        if (debugMode && strobe)
-        {
-            strobeInfo = strobe->getDebugInformation();
+		while (PeekMessage(&Msg, NULL, 0, 0, PM_REMOVE) > 0) {
+            TranslateMessage(&Msg);
+            DispatchMessage(&Msg);
         }
-
-        while (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) > 0)
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-
-        swapChain->Present(1, 0); // VSync is on
     }
 
-    return (int)msg.wParam;
-}
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-    LPSTR lpCmdLine, int nCmdShow)
-{
-    SetThreadPriority(GetCurrentThread(), THREAD_PRIORITY_TIME_CRITICAL);
-
-    HWND hwnd;
-    if (!CreateMainWindow(hInstance, nCmdShow, hwnd)) {
-        return 0;
-    }
-
-    if (!SetupDXGI(hwnd)) {
-        return 0;
-    }
-
-    LPWSTR* szArglist;
-    int argCount;
-    szArglist = CommandLineToArgvW(GetCommandLineW(), &argCount);
-    if (szArglist == NULL)
+    // Cleanup
+    if (strobeInfo)
     {
-        MessageBox(NULL, "Unable to parse command line!", "Error!", MB_ICONEXCLAMATION | MB_OK);
-        return 0;
+        free(strobeInfo);
+        strobeInfo = nullptr;
     }
 
-    if (argCount == 4)
+    if (strobe)
     {
-        int sMode = _wtoi(szArglist[1]);
-        int pSInterval = _wtoi(szArglist[2]);
-        debugMode = _wtoi(szArglist[3]) != 0;
-
-        try {
-            strobe = std::make_unique<StrobeCore>(sMode, pSInterval);
-        }
-        catch (const std::exception& e) {
-            MessageBox(NULL, e.what(), "Error!", MB_ICONEXCLAMATION | MB_OK);
-            LocalFree(szArglist);
-            return 0;
-        }
+        delete strobe;
+        strobe = nullptr;
     }
-    else
+
+    if (dxgiOutput)
     {
-        try {
-            strobe = std::make_unique<StrobeCore>();
-        }
-        catch (const std::exception& e) {
-            MessageBox(NULL, e.what(), "Error!", MB_ICONEXCLAMATION | MB_OK);
-            LocalFree(szArglist);
-            return 0;
-        }
+        dxgiOutput->Release();
+        dxgiOutput = nullptr;
     }
 
-    LocalFree(szArglist);
+    if (dxgiAdapter)
+    {
+        dxgiAdapter->Release();
+        dxgiAdapter = nullptr;
+    }
 
-    int result = RunMainLoop(hwnd);
+    if (dxgiFactory)
+    {
+        dxgiFactory->Release();
+        dxgiFactory = nullptr;
+    }
 
-    // Clean up
-    if (dxgiOutput) dxgiOutput->Release();
-    if (swapChain) swapChain->Release();
+    if (d3d12Device)
+    {
+        d3d12Device->Release();
+        d3d12Device = nullptr;
+    }
 
-    return result;
+    return (int)Msg.wParam;
 }
